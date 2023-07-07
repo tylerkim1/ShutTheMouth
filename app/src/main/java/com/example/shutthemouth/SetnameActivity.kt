7af03ff0479c00aa7660e6ec23c17e711aaaedba
tylerkim <tylerkim129@gmail.com>
2023-07-07 20:38:12 +0900

    make only page

--- app/src/main/java/com/example/shutthemouth/SetnameActivity.kt
+++ app/src/main/java/com/example/shutthemouth/SetnameActivity.kt
@@ -1,11 +1,20 @@
 package com.example.shutthemouth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SetnameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setname)
+
+        val button: Button = findViewById(R.id.setname_button)
+        button.setOnClickListener {
+            // MainActivity로 전환
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/shutthemouth/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/shutthemouth/ui/home/HomeFragment.kt
@@ -12,9 +12,6 @@ import com.example.shutthemouth.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,16 +19,9 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 

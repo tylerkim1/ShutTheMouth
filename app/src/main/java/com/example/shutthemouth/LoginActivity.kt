c1e70f5aa9b9cdf4fbe80830998bd57346dc9246
tylerkim <tylerkim129@gmail.com>
2023-07-07 18:24:14 +0900

    make name page

--- app/src/main/java/com/example/shutthemouth/LoginActivity.kt
+++ app/src/main/java/com/example/shutthemouth/LoginActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.shutthemouth.databinding.ActivityLoginBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -19,11 +21,25 @@ const val TAG = "LoginActivity"
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        imageView = binding.loginGif1
+        Glide.with(this).load(R.raw.bug).into(imageView)
+        imageView = binding.loginGif2
+        Glide.with(this).load(R.raw.dancingman).into(imageView)
+        imageView = binding.loginGif3
+        Glide.with(this).load(R.raw.duck).into(imageView)
+        imageView = binding.loginGif4
+        Glide.with(this).load(R.raw.groot).into(imageView)
+        imageView = binding.loginGif5
+        Glide.with(this).load(R.raw.jola).into(imageView)
+        imageView = binding.loginGif6
+        Glide.with(this).load(R.raw.squid).into(imageView)
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
@@ -53,10 +69,10 @@ class LoginActivity : AppCompatActivity() {
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
             val account = completedTask.getResult(ApiException::class.java)
-
-            binding.loginSignInButton.visibility = View.GONE
-            binding.loginText.visibility = View.VISIBLE
-            binding.loginText.text = account?.displayName
+            // Signed in successfully, navigate to SetnameActivity
+            val intent = Intent(this, SetnameActivity::class.java)
+            startActivity(intent)
+            finish()
         } catch (e: ApiException) {
             Log.w(TAG, "signInResult:failed code=" + e.statusCode)
             binding.loginSignInButton.visibility = View.VISIBLE
---
+++ app/src/main/java/com/example/shutthemouth/SetnameActivity.kt
@@ -0,0 +1,11 @@
+package com.example.shutthemouth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SetnameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setname)
+    }
+}

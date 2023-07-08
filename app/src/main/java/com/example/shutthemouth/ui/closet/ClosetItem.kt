721fe9970200bc3a2ad1df8c7b84a0c315764cda
tylerkim <tylerkim129@gmail.com>
2023-07-08 23:52:52 +0900

    before API

--- app/src/main/java/com/example/shutthemouth/ui/closet/ClosetItem.kt
+++ app/src/main/java/com/example/shutthemouth/ui/closet/ClosetItem.kt
@@ -3,5 +3,6 @@ package com.example.shutthemouth.ui.closet
 data class ClosetItem (
     var img: Int,
     var avatar_img: Int,
-    var name: String
+    var name: String,
+    var background_img: Int
 )

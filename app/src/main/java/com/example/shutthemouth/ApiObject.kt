9e07489e916295c35c6aeaa553fc4bc476c8a56a
tylerkim <tylerkim129@gmail.com>
2023-07-11 15:05:04 +0900

    add user yeah

--- app/src/main/java/com/example/shutthemouth/ApiObject.kt
+++ app/src/main/java/com/example/shutthemouth/ApiObject.kt
@@ -4,7 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiObject {
-    private const val BASE_URL = "http://10.0.2.2:3000"
+    private const val BASE_URL = "http://172.10.5.110:80"
+//    private const val BASE_URL = "http://10.0.2.2:3000"
 
     private val getRetrofit by lazy {
         Retrofit.Builder()

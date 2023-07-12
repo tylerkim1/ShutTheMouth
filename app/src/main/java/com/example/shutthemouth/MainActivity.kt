f5d0b8e282da2adf2b79e31dc9dd4c1fa9ac544a
useruseruse <eliza1102@kaist.ac.kr>
2023-07-12 18:38:20 +0900

    sound

--- app/src/main/java/com/example/shutthemouth/MainActivity.kt
+++ app/src/main/java/com/example/shutthemouth/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.shutthemouth
 
+import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -26,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         banWord = ArrayList<String>(),
         currentRoom = "0"
     )
+    private lateinit var player: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("userId", myData.toString())
@@ -36,9 +39,8 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val  mediaPlayer = MediaPlayer.create(this, R.raw.music);
-        mediaPlayer.start();
+        player = MediaPlayer.create(this, R.raw.music);
+        player.start();
 
         Log.d("this user", myData.toString())
         val data = mapOf<String,User>("user" to myData)
@@ -67,4 +69,12 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         navView.setupWithNavController(navController)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        player.stop()
+    }
+
+
+
 }
